01cb7d937441b3681d6d03d85a19287e3be65c0b
d6nsx <144886207+d6nsx@users.noreply.github.com>
2024-09-19 14:46:01 +0600

    Add files via upload

---
+++ project/js/lesson.js
@@ -0,0 +1,104 @@
+const phoneInput = document.querySelector('#phone_input');
+const phoneButton = document.querySelector('#phone_button');
+const phoneResult = document.querySelector('#phone_result');
+console.log(phoneButton)
+
+const regExp = /^\+996 [25793]\d{2} \d{2}-\d{2}-\d{2}$/
+
+phoneButton.onclick = () => {
+    if (regExp.test(phoneInput.value)) {
+        phoneResult.innerHTML = 'Все правильно!'
+        phoneInput.style.borderColor = 'green'
+        phoneInput.style.Color = 'green'
+        console.log("de")
+    }
+    else {
+        phoneResult.innerHTML = 'Неправильно!'
+        phoneInput.style.borderColor = 'red'
+        phoneInput.style.Color = 'red'
+        console.log("rrr")
+    }
+
+}
+
+const tabСontentBlocks = document.querySelectorAll('.tab_content_block')
+
+const tabContentItems = document.querySelectorAll('.tab_content_item')
+const tabContentItemsParent = document.querySelector('.tab_content_items')
+
+const hideTabContent = () => {
+    tabСontentBlocks.forEach(block => {
+        block.style.display = 'none'
+    })
+    tabContentItems.forEach(item => {
+        item.classList.remove('tab_content_item_active')
+    })
+}
+let current = 0
+
+
+const showTabContent = (index=0 ) => {
+    tabСontentBlocks[index].style.display = 'block'
+    tabContentItems[index].classList.add('tab_content_item_active')
+}
+hideTabContent()
+showTabContent()
+
+tabContentItemsParent.onclick = (event) => {
+    if (event.target.classList.contains('tab_content_item')) {
+        tabContentItems.forEach((item, index) =>{
+            if (event.target === item) {
+                hideTabContent()
+                showTabContent(index)
+                current = index
+            }
+        })
+    }
+}
+
+
+
+setInterval(()=>{
+    if(current< tabСontentBlocks.length){
+
+        hideTabContent()
+        showTabContent(current)
+        current++
+    }else{
+        current=0
+    }
+} , 5000)
+
+
+const somInput = document.querySelector('#som');
+const usdInput = document.querySelector('#usd');
+const wonInput = document.querySelector('#won');
+
+const converter = (element, targetElement1, targetElement2) => {
+    element.oninput = () => {
+        const request = new XMLHttpRequest();
+        request.open('GET', '../data/converter.json');
+        request.send();
+
+        request.onload = () => {
+            const data = JSON.parse(request.response);
+
+            if (element.id === "som") {
+                targetElement1.value = (element.value / data.usd).toFixed(2);
+                targetElement2.value = (element.value / data.won).toFixed(2);
+            }
+            if (element.id === "usd") {
+                targetElement1.value = (element.value * data.usd).toFixed(2);
+                targetElement2.value = (element.value * data.won).toFixed(2);
+            }
+            if (element.id === "won") {
+                targetElement1.value = (element.value / data.won).toFixed(2);
+                targetElement2.value = (element.value / data.usd).toFixed(2);
+            }
+        };
+    };
+};
+
+converter(somInput, usdInput, wonInput);
+converter(usdInput, somInput, wonInput);
+converter(wonInput, somInput, usdInput);
